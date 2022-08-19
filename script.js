a624164d703591355734c38691caa6e96b1567ce
BITMMB <antonrusakov@mail.ru>
2022-08-19 20:49:08 +0300

    Changes

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-function initSlider() {
-    new Swiper('.swiper', {
-        direction: 'horizontal',
-        loop: false,
-        spaceBetween: 170,
-        slidesPerView: 2,
-        pagination: {
-            el: '.swiper-pagination',
-            type: 'bullets',
-        }
-    })
-}
+const slider = document.querySelector('.slider-container');
+let mySwiper;
 
-function detectWidth() {
-    if (window.innerWidth <= 768) {
-        initSlider();
-    } else {
+function mobileSlider() {
+    if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
+        mySwiper = new Swiper(slider, {
+            slidesPerView: 2,
+            spaceBetween: 190,
+            loop: false,
+            slideClass: 'card',
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+        });
+        slider.dataset.mobile = 'true';
+    }
+    if (window.innerWidth > 768) {
+        slider.dataset.mobile = 'false';
+        if (slider.classList.contains('swiper-container-initialized')) {
+            mySwiper.destroy();
+        }
     }
 }
 
-detectWidth();
+mobileSlider()
+window.addEventListener('resize', () => {
+    mobileSlider();
+});
 
 function showMore() {
-    let brandClass = document.querySelector('.brand-buttons');
+    let brandClass = document.querySelector('.swiper-wrapper');
     let brandHeight = window.getComputedStyle(brandClass, null).getPropertyValue("height");
-    let showMoreButton = document.querySelector('.showmore-btn');
     if (brandHeight == '170px') {
-        brandClass.style.height = 'auto';
+        let showMoreButton = document.querySelector('.showmore-btn');
+        brandClass.classList.add('swiper-wrapper__height-auto');
         showMoreButton.textContent = 'Скрыть';
+        showMoreButton.className = 'showmore-btn__hide';
     } else {
-        brandClass.style.height = '170px';
+        let showMoreButton = document.querySelector('.showmore-btn__hide');
+        brandClass.className = 'swiper-wrapper';
         showMoreButton.textContent = 'Показать все';
+        showMoreButton.className = 'showmore-btn';
     }
 }
-
-
-
-
